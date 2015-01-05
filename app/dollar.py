69bcd7122eccad1119d2933670dd3bf575ccdced
Julien <julien.candela@centrale-marseille.fr>
2015-01-05 10:51:50 +0100

    dollar __mul__

--- app/dollar.py
+++ app/dollar.py
@@ -5,5 +5,5 @@ class Dollar:
     def __init__(self, amount):
         self.amount = amount
 
-    def times(self, multiplier):
-        self.amount *= multiplier
+    def __mul__(self, multiplier):
+        return Dollar(self.amount * multiplier)
